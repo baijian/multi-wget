da813d9d5b05fd0680924baa45f54ca4d3e668e4
柏健 <jian.baij@gmail.com>
2013-11-29 16:59:53 +0800

    init

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject multi-wget "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :main ^:skip-aot multi-wget.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
---
+++ test/multi_wget/core_test.clj
@@ -0,0 +1,7 @@
+(ns multi-wget.core-test
+  (:require [clojure.test :refer :all]
+            [multi-wget.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
