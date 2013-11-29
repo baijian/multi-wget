e02617507125cb90a564527ad3e126f0553e7180
柏健 <jian.baij@gmail.com>
2013-11-29 22:41:25 +0800

    fix exit after wget all files

--- src/multi_wget/core.clj
+++ src/multi_wget/core.clj
@@ -9,15 +9,13 @@
       (clojure.contrib.io/to-byte-array
         (clojure.contrib.io/input-stream
           (clojure.contrib.io/as-url from)))))
-  (println (str "wget " (str to (last (.split from "/"))) " suc!")))
+  (str "wget " (str to (last (.split from "/"))) " suc!"))
 
 (defn -main
   "wget binary file in parallel using multi-threads"
   [& args]
-  (let [futures-list 
-        (doall
-          (doseq [from (next args)]
-            (future (wget (nth args 0) from))))
+  (let [futures-list (doall (map #(future (wget (nth args 0) %))(next args)))
         results (map deref futures-list)]
-    (doseq [r results] (println r))))
-;(java.lang.System/exit 0)
+    (doseq [r results] (println r))
+    (java.lang.System/exit 0)))
+
