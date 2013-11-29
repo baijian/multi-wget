d2763de770b50ffc1f3fffcfdeea1cf6974aa28c
柏健 <jian.baij@gmail.com>
2013-11-29 19:10:33 +0800

    done with wget file in threads

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojure-contrib "1.2.0"]]
   :main ^:skip-aot multi-wget.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/multi_wget/core.clj
+++ src/multi_wget/core.clj
@@ -1,7 +1,23 @@
 (ns multi-wget.core
+  (:import [java.io File FileOutputStream])
+  (:require [clojure.contrib.io])
   (:gen-class))
 
+(defn wget [to from]
+  (with-open [out (FileOutputStream. (str to (last (.split from "/"))))]
+    (.write out
+      (clojure.contrib.io/to-byte-array
+        (clojure.contrib.io/input-stream
+          (clojure.contrib.io/as-url from)))))
+  (println (str "wget " (str to (last (.split from "/"))) " suc!")))
+
 (defn -main
-  "I don't do a whole lot ... yet."
+  "wget binary file in parallel using multi-threads"
   [& args]
-  (println "Hello, World!"))
+  (let [futures-list 
+        (doall
+          (doseq [from (next args)]
+            (future (wget (nth args 0) from))))
+        results (map deref futures-list)]
+    (doseq [r results] (println r))))
+;(java.lang.System/exit 0)
